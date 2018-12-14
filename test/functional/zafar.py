c2218f217ca8ecceaaba94feff55239a6107eb87
Shadab Zafar <dufferzafar0@gmail.com>
2018-12-14 10:03:30 +0530

    Add tests to show chainwork updates

--- test/functional/zafar.py
+++ test/functional/zafar.py
@@ -3,6 +3,7 @@
 """Boilerplate copied from mining_basic.py."""
 
 import random
+import time
 
 from pprint import pprint
 
@@ -11,7 +12,7 @@ from binascii import b2a_hex
 from test_framework.blocktools import create_coinbase
 from test_framework.mininode import CBlock
 from test_framework.test_framework import BitcoinTestFramework
-from test_framework.util import assert_equal
+from test_framework.util import assert_equal, disconnect_nodes
 # from test_framework.util import assert_raises_rpc_error
 
 
@@ -105,47 +106,164 @@ class SubmitBlocks(BitcoinTestFramework):
 class SubmitAnchors(BitcoinTestFramework):
 
     def set_test_params(self):
-        # TODO: Test why it doesn't work with a single node
         self.num_nodes = 2
         self.setup_clean_chain = False
 
     def run_test(self):
         node = self.nodes[0]
-
-        node.generate(1)
+        node.generate(5)
         tmpl = node.getblocktemplate()
 
         # Add a block
-        block = new_block(tmpl, 1)
-        node.submitblock(b2x(block.serialize()))
+        # block = new_block(tmpl, 1)
+        # node.submitblock(b2x(block.serialize()))
 
+        # print(node.generateanchor())
+
+        print("Previous Block Hash: ", tmpl["previousblockhash"])
         cw = node.getblockheader(tmpl["previousblockhash"])["chainwork"]
         print("Chainwork, before: ", cw)
 
         block = new_block(tmpl, 1)
         node.submitanchor(b2x(block.serialize()))
 
+        print("Previous Block Hash: ", tmpl["previousblockhash"])
         cw = node.getblockheader(tmpl["previousblockhash"])["chainwork"]
         print("Chainwork, after : ", cw)
 
         # Ask the other node, what the state is
         node = self.nodes[1]
-        tmpl = node.getblocktemplate()
 
+        print("Previous Block Hash: ", tmpl["previousblockhash"])
         cw = node.getblockheader(tmpl["previousblockhash"])["chainwork"]
         print("Chainwork, node 1: ", cw)
 
-        # rsp = node.submitanchor(b2x(block.serialize()))
-        # block = new_block(tmpl, 3)
-        # rsp = node.submitanchor(b2x(block.serialize()))
+        block = new_block(tmpl, 1)
+        node.submitanchor(b2x(block.serialize()))
+
+        time.sleep(random.randint(1, 5))
+
         # block = new_block(tmpl, 4)
         # rsp = node.submitanchor(b2x(block.serialize()))
 
-        # print(rsp)
+        print("Previous Block Hash: ", tmpl["previousblockhash"])
+        cw = node.getblockheader(tmpl["previousblockhash"])["chainwork"]
+        print("Chainwork, node 1: ", cw)
 
-        # assert False
+        # raise False
+
+
+class GenerateAnchors(BitcoinTestFramework):
+
+    def set_test_params(self):
+        self.num_nodes = 2
+        self.setup_clean_chain = False
+
+    def run_test(self):
+        node = self.nodes[0]
+        node.generate(5)
+        tmpl = node.getblocktemplate()
+
+        # print("Previous Block Hash: ", tmpl["previousblockhash"])
+        cw = node.getblockheader(tmpl["previousblockhash"])["chainwork"]
+        print("Chainwork, before: ", cw)
+
+        node.generateanchor()
+        time.sleep(3)
+
+        # print("Previous Block Hash: ", tmpl["previousblockhash"])
+        cw = node.getblockheader(tmpl["previousblockhash"])["chainwork"]
+        print("Chainwork, after : ", cw)
+
+        # Ask the other node, what the state is
+        node = self.nodes[1]
+
+        # print("Previous Block Hash: ", tmpl["previousblockhash"])
+        cw = node.getblockheader(tmpl["previousblockhash"])["chainwork"]
+        print("Chainwork, node 1: ", cw)
+
+        node.generateanchor()
+
+        # time.sleep(random.randint(1, 5))
+
+        # print("Previous Block Hash: ", tmpl["previousblockhash"])
+        cw = node.getblockheader(tmpl["previousblockhash"])["chainwork"]
+        print("Chainwork, node 1: ", cw)
+
+        raise False
+
+
+class CreateForksPy(BitcoinTestFramework):
+
+    def set_test_params(self):
+        self.num_nodes = 2
+        self.setup_clean_chain = False
+
+    def run_test(self):
+        node = self.nodes[0]
+        node.generate(1)
+
+        tmpl = node.getblocktemplate()
+
+        # print(tmpl)
+
+        # print("Previous Block Hash: ", tmpl["previousblockhash"])
+        cw = node.getblockheader(tmpl["previousblockhash"])["chainwork"]
+        print("Chainwork, before: ", tmpl["previousblockhash"], cw, int(cw, 16))
+
+        block = new_block(tmpl, 1)
+        node.submitblock(b2x(block.serialize()))
+
+        cw = node.getblockheader(block.hash)["chainwork"]
+        print("Chainwork, block 1: ", block.hash, cw, int(cw, 16))
+
+        block = new_block(tmpl, 1)
+        node.submitblock(b2x(block.serialize()))
+
+        cw = node.getblockheader(block.hash)["chainwork"]
+        print("Chainwork, block 2: ", block.hash, cw, int(cw, 16))
+
+        print(node.getchaintips())
+
+
+class CreateForksCpp(BitcoinTestFramework):
+
+    def set_test_params(self):
+        self.num_nodes = 2
+        self.setup_clean_chain = False
+
+    def run_test(self):
+        node1 = self.nodes[0]
+        node2 = self.nodes[1]
+
+        # node1.generate(1)
+
+        # Both nodes now have same state
+
+        # Disconnect both nodes
+        disconnect_nodes(self.nodes[0], 1)
+        disconnect_nodes(self.nodes[1], 0)
+
+        b1 = node1.generate(1)[0]
+        b1 = node1.getblockheader(b1)
+        p1 = b1["previousblockhash"]
+        cw1 = b1["chainwork"]
+        b1 = b1["hash"]
+
+        b2 = node2.generate(1)[0]
+        b2 = node2.getblockheader(b2)
+        p2 = b2["previousblockhash"]
+        cw2 = b2["chainwork"]
+        b2 = b2["hash"]
+
+        # Both these blocks should have same chainwork
+        print("block 1: ", p1, b1, int(cw1, 16))
+        print("block 2: ", p2, b2, int(cw2, 16))
 
 
 if __name__ == '__main__':
     # SubmitBlocks().main()
-    SubmitAnchors().main()
+    # SubmitAnchors().main()
+    # GenerateAnchors().main()
+    # CreateForksPy().main()
+    CreateForksCpp().main()
